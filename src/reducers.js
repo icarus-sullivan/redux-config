610d2541b7182677abb6a6e79f439f7d047f0f79
Chris <chrissullivan.dev@gmail.com>
2019-10-10 15:55:04 -0600

    Adding @@redux/INIT initial call, returns null

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,13 @@
+const REDUX_INIT = '@@redux/INIT';
+
 export const createReducer = (config) => {
   const configMap = Array.isArray(config)
     ? config.reduce((a, b) => ({ ...a, ...b }), {})
     : config;
 
   return (state, { type, payload }) => {
+    if (type === REDUX_INIT) return null;
+
     const handler = configMap[type];
     if (handler) {
       return handler(state, payload);
