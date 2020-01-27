481740683a42eaed540c7945855d0bebf99cfcdc
Chris Sullivan <chrissullivan.dev@gmail.com>
2020-01-27 14:15:50 -0800

    Cleaning up comment, modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
-
 export { createActions } from './actions';
 export { createConstants, createAsyncEnum } from './constants';
 export { createReducer } from './reducers';
